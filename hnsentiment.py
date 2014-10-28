7c86d586687730bc7b36fdd7ecbfcbc30839e00b
Ubuntu <ubuntu@ip-172-31-41-158.ec2.internal>
2014-10-28 16:05:33 +0000

    changed to multithreaded model

--- hnsentiment.py
+++ hnsentiment.py
@@ -2,13 +2,14 @@
 
 import requests
 import json
-from multiprocessing import Pool
+import threading
 
 HN_TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
 HN_ITEM_QUERY_BASE_URL = "https://hacker-news.firebaseio.com/v0/item/"
 
 stories = {}
 comments = {}
+
 
 def build_comments(comment_id_list, story_id):
     for comment_id in comment_id_list:
@@ -17,12 +18,20 @@ def build_comments(comment_id_list, story_id):
         if("kids" in comment):
             build_comments(comment["kids"], story_id)
 
-top_story_ids = json.loads(requests.get(HN_TOP_STORIES_URL).text)
-pool = Pool(10)
-for story_id in top_story_ids:
-    story = json.loads(requests.get(HN_ITEM_QUERY_BASE_URL + str(story_id) + ".json").text)
-    stories[story_id] = story
-    comments[story_id] = []
-    print("Building Comments for Story ID " + str(story_id))
-    if("kids" in story):
-        pool.apply_async(build_comments(story["kids"], story_id))
+def build_stories():
+    top_story_ids = json.loads(requests.get(HN_TOP_STORIES_URL).text)
+    threadList = []
+    for story_id in top_story_ids:
+        story = json.loads(requests.get(HN_ITEM_QUERY_BASE_URL + str(story_id) + ".json").text)
+        stories[story_id] = story
+        comments[story_id] = []
+        print("Building Comments for Story ID " + str(story_id))
+        if("kids" in story):
+            thr = threading.Thread(target=build_comments, args=(story["kids"], story_id,))
+            thr.start()
+    print("Waiting for threads to complete")
+    for thre in threadList:
+        thre.join()
+            
+if  __name__ =='__main__':
+    build_stories()
